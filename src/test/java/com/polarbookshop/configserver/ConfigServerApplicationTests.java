e1c60cd00382ed514d6d2c9f8ae6a0a41d7dd475
ymshin80 <siyumy80@gmail.com>
2024-05-21 10:47:31 +0900

    config-server initialize

---
+++ src/test/java/com/polarbookshop/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
